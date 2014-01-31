a638fdaba1fc456ecebe19b5aef7b6de918ec87c
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2014-01-31 23:19:05 +0000

    some more bug fixes
    - detaching a component only if it's PID is nonempty
      (otherwise open master screen was detached)
    - do not iconize when -d option was given (to detach after some time)
    - removed obsolete files vdemo.in, vdemo_controller.tcl
    - cleanup

--- vdemo_base.sh
+++ vdemo_base.sh
@@ -7,35 +7,35 @@ function start_Xserver {
     i=0;
     echo "trying to find an X display" >&2
     while [ $i -lt 10 ]; do
-	if [ -e "/tmp/.X${i}-lock" ]; then
-	    echo "X running on :${i}, try to use it" >&2
-	    if xwininfo -display :${i} -root 1>&2; then
-		echo "found accessible X :${i}, reusing it" >&2
-		echo ":${i}"
-		return 0
-	    fi
-	else
-	    echo "found :${i} free, starting X server" >&2
-	    X $VDEMO_Xopts :${i} 1>&2 &
-	    echo "$!" > "${XPIDFILE}"
-	    echo "DeskTopSize 1x1" >  /tmp/dummy-fvwm2-config_${USER} 2>&1
-	    echo "X started, waiting before starting window manager" >&2
+		  if [ -e "/tmp/.X${i}-lock" ]; then
+				echo "X running on :${i}, try to use it" >&2
+				if xwininfo -display :${i} -root 1>&2; then
+					 echo "found accessible X :${i}, reusing it" >&2
+					 echo ":${i}"
+					 return 0
+				fi
+		  else
+				echo "found :${i} free, starting X server" >&2
+				X $VDEMO_Xopts :${i} 1>&2 &
+				echo "$!" > "${XPIDFILE}"
+				echo "DeskTopSize 1x1" >  /tmp/dummy-fvwm2-config_${USER} 2>&1
+				echo "X started, waiting before starting window manager" >&2
 	    # This waits until the server is ready
-	    while ! xsetroot -solid black -display ":${i}"; do sleep 1; done
+				while ! xsetroot -solid black -display ":${i}"; do sleep 1; done
 	    # start the fvwm window manager in default mode:
-	    echo "starting fvwm2" >&2
-	    fvwm2 -f /tmp/dummy-fvwm2-config_${USER} -display ":${i}" 1>&2 &
-	    echo "started fvwm2" >&2
-	    sleep 1
+				echo "starting fvwm2" >&2
+				fvwm2 -f /tmp/dummy-fvwm2-config_${USER} -display ":${i}" 1>&2 &
+				echo "started fvwm2" >&2
+				sleep 1
 	    # screen saver off
-	    xset -display ":${i}" s off
-	    xset -display ":${i}" -dpms
-	    xsetroot -solid black -display ":${i}"
-	    echo ":${i}"
-	    return 0
-	fi
-	echo  "X on :${i} locked, try next..." >&2
-	i=`expr $i + 1`
+				xset -display ":${i}" s off
+				xset -display ":${i}" -dpms
+				xsetroot -solid black -display ":${i}"
+				echo ":${i}"
+				return 0
+		  fi
+		  echo  "X on :${i} locked, try next..." >&2
+		  i=`expr $i + 1`
     done
     echo "couldn't find a free display for the xserver. exiting" >&2
     exit 2
@@ -63,20 +63,20 @@ function vdemo_check_component {
 
     VDEMO_pidfile=/tmp/VDEMO_component_${VDEMO_title}_${USER}.pid
     if [ "$VDEMO_pid" ]; then
-	echo "checking $VDEMO_title" >&2
-	if ps ax | grep "^ *${VDEMO_pid}" ; then
-	    echo "running" >&2
-	    echo "$VDEMO_pid" > $VDEMO_pidfile 
-	    return 0
-	else
-	    echo "not running" >&2
-	    rm -f "$VDEMO_pidfile"
-	    return 2
-	fi
+		  echo "checking $VDEMO_title" >&2
+		  if ps ax | grep "^ *${VDEMO_pid}" ; then
+				echo "running" >&2
+				echo "$VDEMO_pid" > $VDEMO_pidfile 
+				return 0
+		  else
+				echo "not running" >&2
+				rm -f "$VDEMO_pidfile"
+				return 2
+		  fi
     else
-	echo "no screen registered" >&2
-	rm -f "$VDEMO_pidfile"
-	return 1
+		  echo "no screen registered" >&2
+		  rm -f "$VDEMO_pidfile"
+		  return 1
     fi
 }
 
@@ -111,7 +111,7 @@ function vdemo_reattach_screen {
 # $1:   title of the component
 function vdemo_detach_screen {
 	 VDEMO_pid=`vdemo_pidFromScreen $1`
-    screen -d "$VDEMO_pid"
+	 test -n "$VDEMO_pid" && screen -d "$VDEMO_pid"
 }
 
 # show log output of a component
@@ -135,46 +135,46 @@ function vdemo_start_component {
     VDEMO_logging=""
     ICONIC="-iconic"
     while [ $# -gt 0 ]; do
-	case $1 in
-	    "-n"|"--name")
-		shift
-		VDEMO_title="$1"
-		;;
-	    "-d"|"--display")
-		shift
-		VDEMO_componentDisplay="$1"
-		;;
-	    "-l"|"--logging")
-		VDEMO_logfile="${VDEMO_logfile_prefix}${VDEMO_title}_${USER}.log"
-		echo "logging to ${VDEMO_logfile}" >&2
-		VDEMO_logging=" | tee ${VDEMO_logfile}"
-		;;
-	    --)
-		break
-		;;
-	    "--noiconic")
-      ICONIC=""
-		;;
-	    -*)
-		echo "illegal option $1" >& 2
-		echo "$USAGE" >& 2
-		exit 1
-		;;
-	    *)	    
-		break
-		;;
-	esac
-	shift
+		  case $1 in
+				"-n"|"--name")
+					 shift
+					 VDEMO_title="$1"
+					 ;;
+				"-d"|"--display")
+					 shift
+					 VDEMO_componentDisplay="$1"
+					 ;;
+				"-l"|"--logging")
+					 VDEMO_logfile="${VDEMO_logfile_prefix}${VDEMO_title}_${USER}.log"
+					 echo "logging to ${VDEMO_logfile}" >&2
+					 VDEMO_logging=" | tee ${VDEMO_logfile}"
+					 ;;
+				--)
+					 break
+					 ;;
+				"--noiconic")
+					 ICONIC=""
+					 ;;
+				-*)
+					 echo "illegal option $1" >& 2
+					 echo "$USAGE" >& 2
+					 exit 1
+					 ;;
+				*)	    
+					 break
+					 ;;
+		  esac
+		  shift
     done
 
     if [ $# -lt 1 ]; then
-	echo "obligatory arguments missing." >&2 
-	return 1
+		  echo "obligatory arguments missing." >&2 
+		  return 1
     fi
     VDEMO_component="$1"
     
     if  [ -z $VDEMO_title ]; then
-	VDEMO_title=`basename VDEMO_component`
+		  VDEMO_title=`basename VDEMO_component`
     fi
 
     cmd="LD_LIBRARY_PATH=${LD_LIBRARY_PATH} DISPLAY=${VDEMO_componentDisplay} $* 2>&1 ${VDEMO_logging}"
@@ -182,8 +182,8 @@ function vdemo_start_component {
     echo "starting $VDEMO_title as '$cmd' " >&2
     
     xterm -fg green -bg black $ICONIC -title "starting $VDEMO_title" -e \
-	screen -t "$VDEMO_title" -S "${VDEMO_title}_" \
-	/bin/bash -i -c "$cmd" &
+		  screen -t "$VDEMO_title" -S "${VDEMO_title}_" \
+		  /bin/bash -i -c "$cmd" &
 }
 
 # get all direct and indirect children of a process
@@ -191,25 +191,25 @@ function all_children {
     PIDS="$*"
     CHILDREN=""
     for P in $PIDS; do
-	echo -n " $P"
-	curr_children=`/bin/ps --ppid $P -o pid --no-headers`
-	if [ "$curr_children" ]; then
-	    all_children "$curr_children"
-	fi
+		  echo -n " $P"
+		  curr_children=`/bin/ps --ppid $P -o pid --no-headers`
+		  if [ "$curr_children" ]; then
+				all_children "$curr_children"
+		  fi
     done
 }
 
 # stop a component
-# $1: titl of the component
+# $1: title of the component
 function vdemo_stop_component {
     VDEMO_title="$1"
     VDEMO_pid=`vdemo_pidFromScreen ${VDEMO_title}`
 
     if [ "$VDEMO_pid" ]; then
-	PIDS=`all_children "$VDEMO_pid"`
-	echo "stopping $VDEMO_title (PIDs $PIDS)" >&2
-	kill $VDEMO_pid $PIDS > /dev/null 2>&1
-	sleep 1
-	kill -9 $VDEMO_pid $PIDS > /dev/null 2>&1
+		  PIDS=`all_children "$VDEMO_pid"`
+		  echo "stopping $VDEMO_title (PIDs $PIDS)" >&2
+		  kill $VDEMO_pid $PIDS > /dev/null 2>&1
+		  sleep 1
+		  kill -9 $VDEMO_pid $PIDS > /dev/null 2>&1
     fi
 }
--- vdemo_standard_component_suffix.sh
+++ vdemo_standard_component_suffix.sh
@@ -1,51 +1,8 @@
 #!/bin/bash
-# ///
-# ///
 # /// @file	vdemo_component_memoryserver.sh
 # /// @author	Marc Hanheide (mhanheid@TechFak.Uni-Bielefeld.de)
 # /// @date	started at 2005/03/01
 # /// @version	$Id: vdemo_standard_component_suffix.sh,v 1.4 2007/08/17 15:28:47 mhanheid Exp $
-# ///
-# /// @brief       vdemo_component_memoryserver.sh not documented yet (read source yourself ;-) )
-# /// 
-# /// 
-# /// $Log: vdemo_standard_component_suffix.sh,v $
-# /// Revision 1.4  2007/08/17 15:28:47  mhanheid
-# /// some forgotten changes and xterm with better colors
-# ///
-# /// Revision 1.3  2006/11/07 16:39:22  mhanheid
-# /// *** empty log message ***
-# ///
-# /// Revision 1.2  2006/07/21 12:52:15  mhanheid
-# /// fixed bug in vdemo_component single start
-# ///
-# /// Revision 1.1  2005/11/24 15:59:05  mhanheid
-# /// initial
-# ///
-# /// Revision 1.7  2005/06/13 09:24:33  mhanheid
-# /// - better screening support
-# ///
-# /// Revision 1.6  2005/05/03 12:32:58  mhanheid
-# /// *** empty log message ***
-# ///
-# /// Revision 1.5  2005/05/03 12:28:58  mhanheid
-# /// *** empty log message ***
-# ///
-# /// Revision 1.4  2005/05/03 12:19:57  mhanheid
-# /// *** empty log message ***
-# ///
-# /// Revision 1.1  2005/04/06 11:08:23  mhanheid
-# /// Initial revision
-# ///
-# /// Revision 1.2  2005/03/09 14:47:17  mhanheid
-# /// small bugfixes and minor changes
-# ///
-# /// Revision 1.1  2005/03/03 17:37:32  mhanheid
-# /// init
-# ///
-# /// Revision 1.1  2005/01/05 10:47:16  mhanheid
-# /// Initial revision
-# ///
 # ///
 
 # ************************************* START *********************************
@@ -78,27 +35,27 @@ vdemo_start_ICONIC=""
 
 while [ $# -gt 0 ]; do
     case $1 in
-	"-h"|"--help")
-	    echo "$HELP" >& 2
-	    exit 0
-	    ;;
-	"-l"|"--logging")
-	    vdemo_start_LOGGING="-l"
-	    ;;
-	"-x"|"--xserver")
-	    vdemo_start_XSERVER="YES"
-	    ;;
-	"--noiconic")
-	    vdemo_start_ICONIC="--noiconic"
-	    ;;
-	-*)
-	    echo "illegal option '$1'" >& 2
-	    echo "$HELP" >& 2
-	    exit 1
-	    ;;
-	*)	    
-	    break
-	    ;;
+		  "-h"|"--help")
+				echo "$HELP" >& 2
+				exit 0
+				;;
+		  "-l"|"--logging")
+				vdemo_start_LOGGING="-l"
+				;;
+		  "-x"|"--xserver")
+				vdemo_start_XSERVER="YES"
+				;;
+		  "--noiconic")
+				vdemo_start_ICONIC="--noiconic"
+				;;
+		  -*)
+				echo "illegal option '$1'" >& 2
+				echo "$HELP" >& 2
+				exit 1
+				;;
+		  *)	    
+				break
+				;;
     esac
     shift
 done
@@ -116,8 +73,8 @@ if [ -z "$VDEMO_root" ]; then
 fi
 
 if [ -z "$title" ]; then
-	echo '$title is not set. A good default is the name of the process' >&2
-	exit 1
+	 echo '$title is not set. A good default is the name of the process' >&2
+	 exit 1
 fi
 
 # run the 
@@ -130,82 +87,82 @@ source "$VDEMO_root/vdemo_base.sh"
 # returns: If function exists: Return value of function.
 #          If function does not exist: True.
 function call_if_exists {
-  func=$1; shift
-  if declare -f $func >/dev/null; then
-    $func $@
-  else
-    true
-  fi
+	 func=$1; shift
+	 if declare -f $func >/dev/null; then
+		  $func $@
+	 else
+		  true
+	 fi
 }
 
 case "$1" in
     start)
-	if vdemo_check_component $title; then
- 	    echo "$title already running, stopping first">&2
- 	    vdemo_stop_component $title
-	    sleep 1
-	fi
+		  if vdemo_check_component $title; then
+ 				echo "$title already running, stopping first">&2
+ 				vdemo_stop_component $title
+				sleep 1
+		  fi
 
-	call_if_exists clean_component
-	call_if_exists on_start
+		  call_if_exists clean_component
+		  call_if_exists on_start
 
-	if [ "${vdemo_start_XSERVER}" ]; then
-	    comp_display=`start_Xserver`
-	    echo "DISPLAY: $comp_display" >&2 
-	    vdemo_start_component -d $comp_display -n $title $vdemo_start_ICONIC $vdemo_start_LOGGING \
-		$component
-	else
-	    vdemo_start_component -n $title $vdemo_start_LOGGING $vdemo_start_ICONIC \
-		$component
-	fi
-	;;
+		  if [ "${vdemo_start_XSERVER}" ]; then
+				comp_display=`start_Xserver`
+				echo "DISPLAY: $comp_display" >&2 
+				vdemo_start_component -d $comp_display -n $title $vdemo_start_ICONIC $vdemo_start_LOGGING \
+					 $component
+		  else
+				vdemo_start_component -n $title $vdemo_start_LOGGING $vdemo_start_ICONIC \
+					 $component
+		  fi
+		  ;;
     stop)
-	vdemo_stop_component $title
-	if [ "${vdemo_start_XSERVER}" ]; then
-	    stop_Xserver
-	fi
-	call_if_exists on_stop
-	;;
+		  vdemo_stop_component $title
+		  if [ "${vdemo_start_XSERVER}" ]; then
+				stop_Xserver
+		  fi
+		  call_if_exists on_stop
+		  ;;
     check)
-	call_if_exists on_check || true
-	vdemo_check_component $title
-	exit $?
-	;;
+		  call_if_exists on_check || true
+		  vdemo_check_component $title
+		  exit $?
+		  ;;
     screen)
-	vdemo_reattach_screen $title
-	exit $?
-	;;
+		  vdemo_reattach_screen $title
+		  exit $?
+		  ;;
     detach)
-	vdemo_detach_screen $title
-	exit $?
-	;;
+		  vdemo_detach_screen $title
+		  exit $?
+		  ;;
     showlog)
-	vdemo_showlog $title
-	exit $?
-	;;
+		  vdemo_showlog $title
+		  exit $?
+		  ;;
     clean)
-	if vdemo_check_component; then
-	    echo "$title is running, stopping before cleaning">&2
-	    vdemo_stop_component $title
-	fi
-	call_if_exists clean_component
-	;;
+		  if vdemo_check_component; then
+				echo "$title is running, stopping before cleaning">&2
+				vdemo_stop_component $title
+		  fi
+		  call_if_exists clean_component
+		  ;;
     single)
-	if [ "$2" ]; then
-	    echo "configuration from $2" >&2
-	    VDEMO_demoConfig="$2"
-	    export VDEMO_demoConfig
-	    source "$2"
-	fi
-	call_if_exists clean_component
-	call_if_exists on_start
-	bash -c "$component"
-	call_if_exists on_stop
-	;;
+		  if [ "$2" ]; then
+				echo "configuration from $2" >&2
+				VDEMO_demoConfig="$2"
+				export VDEMO_demoConfig
+				source "$2"
+		  fi
+		  call_if_exists clean_component
+		  call_if_exists on_start
+		  bash -c "$component"
+		  call_if_exists on_stop
+		  ;;
     *)
-	echo "wrong argument. $HELP" >&2 
-	exit 1
-	;;
+	 echo "wrong argument. $HELP" >&2 
+	 exit 1
+	 ;;
 esac
 
 
