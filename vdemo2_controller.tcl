8bd096df735f5374295a0feceff10e18b9dbac2b
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2015-08-31 10:12:24 +0200

    removed sleep()
    multiple sleeps in a row are not possible anyway:
    only the latest call to vwait stays active:
    https://groups.google.com/forum/#!topic/comp.lang.tcl/PrvhgVmo76w

--- vdemo2_controller.tcl
+++ vdemo2_controller.tcl
@@ -1501,22 +1501,6 @@ wm protocol . WM_DELETE_WINDOW {
     gui_exit
 }
 
-# event-handling sleep, see http://www2.tcl.tk/933
-proc uniqkey { } {
-    set key   [ expr { pow(2,31) + [ clock clicks ] } ]
-    set key   [ string range $key end-8 end-3 ]
-    set key   [ clock seconds ]$key
-    return $key
-}
-
-proc sleep { ms } {
-    set uniq [ uniqkey ]
-    set ::__sleep__tmp__$uniq 0
-    after $ms set ::__sleep__tmp__$uniq 1
-    vwait ::__sleep__tmp__$uniq
-    unset ::__sleep__tmp__$uniq
-}
-
 proc setup_temp_dir { } {
     global TEMPDIR
     set TEMPDIR [exec mktemp -d /tmp/vdemo.XXXXXXXXXX]
