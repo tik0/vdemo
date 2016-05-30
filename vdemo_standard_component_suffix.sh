65dc823d9a6ed8056ff86f18dcf45d0b031ed2c8
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2016-05-30 13:34:20 +0200

    whitespace cleanup

--- vdemo_standard_component_suffix.sh
+++ vdemo_standard_component_suffix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# /// @author	Marc Hanheide (mhanheid@TechFak.Uni-Bielefeld.de)
-# /// @date	started at 2005/03/01
-# /// @version	$Id: vdemo_standard_component_suffix.sh,v 1.4 2007/08/17 15:28:47 mhanheid Exp $
+# /// @author    Marc Hanheide (mhanheid@TechFak.Uni-Bielefeld.de)
+# /// @date      started at 2005/03/01
+# /// @version   $Id: vdemo_standard_component_suffix.sh,v 1.4 2007/08/17 15:28:47 mhanheid Exp $
 #
 # THIS SCRIPT EXPECTS THE FOLLOWING VARIABLES TO BE SET:
 #   component="$prefix/bin/some_binary args"
@@ -27,59 +27,59 @@ export VDEMO_component_title="$title"
 
 while [ $# -gt 0 ]; do
     case $1 in
-	"-h"|"--help")
-	    echo "$HELP" >& 2
-	    exit 0
-	    ;;
-	"-l"|"--logging")
-	    vdemo_start_LOGGING="-l"
-	    ;;
-	"-x"|"--xserver")
-	    vdemo_start_XSERVER="YES"
-	    ;;
-	"-t"|"--title")
-		shift
-		if [ -z "$title" ]; then
-			title="$1"
-		else
-			title="$title.$1"
-            export VDEMO_component_title="$1"
-		fi
-		;;
-	"-D"|"--detached")
-	    vdemo_start_DETACHED="-D"
-	    ;;
-	-*)
-	    echo "illegal option '$1'" >& 2
-	    echo "$HELP" >& 2
-	    exit 3
-	    ;;
-	*)
-	    break
-	    ;;
+        "-h"|"--help")
+            echo "$HELP" >& 2
+            exit 0
+            ;;
+        "-l"|"--logging")
+            vdemo_start_LOGGING="-l"
+            ;;
+        "-x"|"--xserver")
+            vdemo_start_XSERVER="YES"
+            ;;
+        "-t"|"--title")
+            shift
+            if [ -z "$title" ]; then
+               title="$1"
+            else
+               title="$title.$1"
+               export VDEMO_component_title="$1"
+            fi
+            ;;
+        "-D"|"--detached")
+            vdemo_start_DETACHED="-D"
+            ;;
+        -*)
+            echo "illegal option '$1'" >& 2
+            echo "$HELP" >& 2
+            exit 3
+            ;;
+        *)
+            break
+            ;;
     esac
     shift
 done
 
 # obligatory arguments check
 if [ $# -lt 1 ]; then
-    echo "obligatory argument(s) missing. $HELP" >&2
-    exit 3
+   echo "obligatory argument(s) missing. $HELP" >&2
+   exit 3
 fi
 
 # This MUST be the absolut path to the used vdemo scripts (this script)
 if [ -z "$VDEMO_root" ]; then
-    echo '$VDEMO_root is not set. Set it to absolut path of the used vdemo installation' >&2
-    exit 3
+   echo '$VDEMO_root is not set. Set it to absolut path of the used vdemo installation' >&2
+   exit 3
 fi
 
 if [ -z "$VDEMO_logfile_prefix" ]; then
-	export VDEMO_logfile_prefix="/tmp/vdemo-$USER/component_"
+   export VDEMO_logfile_prefix="/tmp/vdemo-$USER/component_"
 fi
 
 if ! declare -F "component" >/dev/null; then
-	echo 'function "component" is not declared or declared as variable'
-	exit 11
+   echo 'function "component" is not declared or declared as variable'
+   exit 11
 fi
 
 # run the
@@ -92,91 +92,91 @@ source "$VDEMO_root/vdemo_base.sh"
 # returns: If function exists: Return value of function.
 #          If function does not exist: True.
 function call_if_exists {
-  func=$1; shift
-  if declare -F $func >/dev/null; then
-    $func $@
-  else
-    true
-  fi
+    func=$1; shift
+    if declare -F $func >/dev/null; then
+       $func $@
+    else
+       true
+    fi
 }
 
 case "$1" in
     start)
-	if vdemo_check_component $title; then
-	    echo "$title already running">&2
-		exit 10
-	fi
+        if vdemo_check_component $title; then
+           echo "$title already running">&2
+           exit 10
+        fi
 
-	call_if_exists clean_component
-	call_if_exists on_start
+        call_if_exists clean_component
+        call_if_exists on_start
 
-	if [ "${vdemo_start_XSERVER}" ]; then
-	    comp_display=$(start_Xserver)
-		 if [ $? == 2 ]; then exit 12; fi
-	    echo "DISPLAY: $comp_display" >&2
-	    vdemo_start_component -n $title $vdemo_start_LOGGING $vdemo_start_DETACHED -d $comp_display
-	else
-	    vdemo_start_component -n $title $vdemo_start_LOGGING $vdemo_start_DETACHED
-	fi
-	;;
+        if [ "${vdemo_start_XSERVER}" ]; then
+           comp_display=$(start_Xserver)
+           if [ $? == 2 ]; then exit 12; fi
+           echo "DISPLAY: $comp_display" >&2
+           vdemo_start_component -n $title $vdemo_start_LOGGING $vdemo_start_DETACHED -d $comp_display
+        else
+           vdemo_start_component -n $title $vdemo_start_LOGGING $vdemo_start_DETACHED
+        fi
+        ;;
     stop)
-			vdemo_stop_component $title &
-	;;
-	stopwait)
-			echo "stopping and waiting" >&2
-			vdemo_stop_component $title
-	;;
+        vdemo_stop_component $title &
+        ;;
+    stopwait)
+        echo "stopping and waiting" >&2
+        vdemo_stop_component $title
+        ;;
     check)
-	vdemo_check_component $title; processResult=$?
-	re="^on_check[^{]*\{[[:space:]]*true[[:space:]]*\}$"
-	func=$(declare -f on_check)
-	if [[ -n $func && ! $func =~ $re ]] ; then
-		# only call on_check if it is defined and non-trivial
-		on_check; callResult=$?
-		# combined result is limited to 8bit!
-		if (( $callResult > 25 || $callResult < 0 )); then callResult=25; fi
-	else
-		callResult=$processResult
-	fi
-	exit $((10*callResult + processResult)) # combine both results
-	;;
+        vdemo_check_component $title; processResult=$?
+        re="^on_check[^{]*\{[[:space:]]*true[[:space:]]*\}$"
+        func=$(declare -f on_check)
+        if [[ -n $func && ! $func =~ $re ]] ; then
+           # only call on_check if it is defined and non-trivial
+           on_check; callResult=$?
+           # combined result is limited to 8bit!
+           if (( $callResult > 25 || $callResult < 0 )); then callResult=25; fi
+        else
+           callResult=$processResult
+        fi
+        exit $((10*callResult + processResult)) # combine both results
+        ;;
     screen)
-	vdemo_reattach_screen $title
-	exit $?
-	;;
+        vdemo_reattach_screen $title
+        exit $?
+        ;;
     detach)
-	vdemo_detach_screen $title
-	exit $?
-	;;
+        vdemo_detach_screen $title
+        exit $?
+        ;;
     showlog)
-	vdemo_showlog $title
-	exit $?
-	;;
+        vdemo_showlog $title
+        exit $?
+        ;;
     inspect)
-	vdemo_inspect $title
-	;;
+        vdemo_inspect $title
+        ;;
     clean)
-	if vdemo_check_component; then
-	    echo "$title is running, stopping before cleaning">&2
-	    vdemo_stop_component $title
-	fi
-	call_if_exists clean_component
-	;;
+        if vdemo_check_component; then
+           echo "$title is running, stopping before cleaning">&2
+           vdemo_stop_component $title
+        fi
+        call_if_exists clean_component
+        ;;
     single)
-	if [ "$2" ]; then
-	    echo "configuration from $2" >&2
-	    VDEMO_demoConfig="$2"
-	    export VDEMO_demoConfig
-	    source "$2"
-	fi
-	call_if_exists clean_component
-	call_if_exists on_start
-	export -f component
-	bash -c "component"
-	call_if_exists on_stop
-	;;
+        if [ "$2" ]; then
+           echo "configuration from $2" >&2
+           VDEMO_demoConfig="$2"
+           export VDEMO_demoConfig
+           source "$2"
+        fi
+        call_if_exists clean_component
+        call_if_exists on_start
+        export -f component
+        bash -c "component"
+        call_if_exists on_stop
+        ;;
     *)
-	echo "wrong argument. $HELP" >&2
-	exit 3
-	;;
+        echo "wrong argument. $HELP" >&2
+        exit 3
+        ;;
 esac
