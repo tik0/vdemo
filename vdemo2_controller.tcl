2ed582e2d68b4ae5bff048ba9ec1255acb4e9ea2
Viktor Richter <vrichter@posteo.de>
2017-10-26 16:43:56 +0200

    Make VDEMO_CONNCHECK_TIMEOUT accesible in function

--- vdemo2_controller.tcl
+++ vdemo2_controller.tcl
@@ -1270,6 +1270,7 @@ proc communicate_ssh {host cmd {timeout 0}} {
 }
 
 proc ssh_check_connection {hostname {connect 1}} {
+    global VDEMO_CONNCHECK_TIMEOUT
     set fifo [get_fifo_name $hostname]
     # error code to indicate missing master connection
     set res -1
