45001c16c719a2bbb45aa4f1eeec6ae0aae85a92
Lars Schillingmann <lars@schillingmann.net>
2017-11-13 23:50:22 +0100

    dummy component for spread testing

---
+++ examples/example_spread.sh
@@ -0,0 +1,36 @@
+source "$VDEMO_demoRoot/example_config.sh"
+
+# instead of launching vdemo2 with -s we specify the port here to allow for
+# different ports for different configurations
+export VDEMO_SERVER_PORT=4443
+export VDEMO_SERVER_KEY=demo
+
+host01="vdemohost01"
+host02="vdemohost02"
+host03="vdemohost03"
+
+##############################################################################
+# COMPONENT LIST
+##############################################################################
+#   Options:
+#      -w <n> wait n seconds for process to start completely
+#      -W <n> set delay an asynchronous check is performed in case -w is not specified
+#      -l     activate logging for the component
+#      -x     detect and use remote X server for the component
+#      -n     do not include in autostart
+#      -g <s> allow to define a group (string: name of group)
+#      -L <n> component level, affects starting order (numeric: level)
+#      -d <n> detach time, automatically detaches screen after n seconds, or
+#             leaves it open all the time (-1), default is 10 seconds
+#      -t     title of the component / provide unique names for multiple instances on the same host
+#      -v     export variable varname=var to component script
+#      -r     ask to restart component on crash
+#      -R     automatically component on crash
+#      -Q     Quit vdemo if component terminates
+
+
+export VDEMO_components="
+spreaddaemon,             $host01,            -t spread -L 1  -g sleepers -w 0 -W 1 -x:
+spreaddaemon,             $host02,            -t spread -L 1  -g sleepers -w 0 -W 1 -x:
+spreaddaemon,             $host03,            -t spread -L 1  -g sleepers -w 0 -W 1 -x:
+"
